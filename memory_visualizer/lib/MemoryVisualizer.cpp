2d94721269e518b0c292d667ab602d8126b1ed42
Elisa Fröhlich <elisaa.frohlich@gmail.com>
2026-03-20 14:39:20 -0300

    Finding uses of alloc instructions

--- memory_visualizer/lib/MemoryVisualizer.cpp
+++ memory_visualizer/lib/MemoryVisualizer.cpp
@@ -17,16 +17,30 @@ namespace {
     void runOnOperation() override {
       ModuleOp module = getOperation();
   
+      
+      
       // Iterate over all functions in the module
       for (auto funcOp : module.getOps<func::FuncOp>()) {
         // Function name
         llvm::outs() << "Function: " << funcOp.getName() << "\n";
+        
+        llvm::DenseMap<Operation*,int64_t> instructionMapping;
+        int64_t idx = 0;
+        Block &bodyBlock = funcOp.getBody().front();
+        funcOp.walk([&](Operation *op) {
+          llvm::outs() << idx << ": " << *op << "\n";
+          instructionMapping[op] = idx++;
+        });
   
+        llvm::outs() << "\n\n";
+
         for (auto allocOp : funcOp.getOps<memref::AllocOp>()) {
-          llvm::outs() << "Found alloc instruction: " << allocOp << "\n";
-
           auto type = allocOp.getResult().getType();
+          auto result = allocOp.getResult();
+          
+          llvm::outs() << "Found alloc instruction at idx: " << instructionMapping.at(result.getDefiningOp()) << "\n\n";
 
+          // Size computation
           int64_t numElements = 1;
           for (auto dim : type.getShape()) {
             if (dim == ShapedType::kDynamic) dim = 1;
@@ -36,12 +50,21 @@ namespace {
           auto elemType = type.getElementType();
           auto typeSize = elemType.getIntOrFloatBitWidth() / 8;
 
+          // Size information
           llvm::outs() << "\tSize: " << numElements*typeSize << "\n";
-
+          
           llvm::outs() << "\t\tType: " << type << "\n";
           llvm::outs() << "\t\tElement Type: " << elemType << "\n";
           llvm::outs() << "\t\tNumber of Elements: " << numElements << "\n";
-          llvm::outs() << "\t\tType size: " << typeSize << "\n";
+          llvm::outs() << "\t\tType size: " << typeSize << "\n\n";
+
+          // Usage information
+          llvm::outs() << "\tUsed at:\n";
+          for (auto user : result.getUsers()) {
+            llvm::outs() << instructionMapping.at(user) << ": ";
+            llvm::outs() << *user << "\n";
+          }
+          llvm::outs() << "\n\n"; 
         }
       }
     }
