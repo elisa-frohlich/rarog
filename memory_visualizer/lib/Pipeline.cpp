930a18fc6fef3e3d0078214ec563e363b52d4dd1
Elisa Fröhlich <elisaa.frohlich@gmail.com>
2026-03-23 11:02:43 -0300

    Adding passes for using buffer-deallocation-pipeline

--- memory_visualizer/lib/Pipeline.cpp
+++ memory_visualizer/lib/Pipeline.cpp
@@ -3,8 +3,10 @@
 
 #include "mlir/Dialect/Bufferization/Transforms/OneShotAnalysis.h"
 #include "mlir/Dialect/Bufferization/Transforms/Passes.h"
+#include "mlir/Dialect/Bufferization/Transforms/Bufferize.h"
 #include "mlir/Dialect/Linalg/Passes.h"
 #include "mlir/Dialect/MemRef/Transforms/Passes.h"
+#include "mlir/Transforms/Passes.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Pass/PassOptions.h"
 #include "mlir/Pass/PassRegistry.h"
@@ -15,11 +17,33 @@ using namespace mlir;
 namespace {
 
 void addMemoryVisualizerPipeline(OpPassManager &pm) {
-    mlir::bufferization::OneShotBufferizePassOptions bufferizationOptions;
+
+    // --one-shot-bufferize="bufferize-function-boundaries"
+    bufferization::OneShotBufferizePassOptions bufferizationOptions;
     bufferizationOptions.bufferizeFunctionBoundaries = true;
     pm.addPass(
-        mlir::bufferization::createOneShotBufferizePass(bufferizationOptions)
+        bufferization::createOneShotBufferizePass(bufferizationOptions)
     );
+
+    // // --buffer-deallocation-pipeline
+    // memref::ExpandReallocPassOptions expandAllocPassOptions{
+    //   /*emitDeallocs=*/false};
+    // pm.addPass(memref::createExpandReallocPass(expandAllocPassOptions));
+    // pm.addPass(createCanonicalizerPass());
+
+    // bufferization::OwnershipBasedBufferDeallocationPassOptions deallocationOptions;
+    // deallocationOptions.privateFuncDynamicOwnership = true;
+
+    // pm.addPass(
+    //     bufferization::createOwnershipBasedBufferDeallocationPass(deallocationOptions)
+    // );
+    // pm.addPass(createCanonicalizerPass());
+    // pm.addPass(bufferization::createBufferDeallocationSimplificationPass());
+    // pm.addPass(bufferization::createLowerDeallocationsPass());
+    // pm.addPass(createCSEPass());
+    // pm.addPass(createCanonicalizerPass());
+
+    // --memory-visualizer
     pm.addPass(rarog::createMemoryVisualizerPass());
 }
 
