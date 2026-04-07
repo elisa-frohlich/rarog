8708269fbf8672ef55ca982df348bf1d4e285290
bernborgess <bernborgess@outlook.com>
2026-04-07 17:23:13 -0300

    [sn] Adding comments for toposort

--- utils/topological_ordering_count.cpp
+++ utils/topological_ordering_count.cpp
@@ -1,12 +1,16 @@
-// Following the ideas behind this site: https://math.stackexchange.com/questions/3220701/is-it-possible-to-calculate-how-many-topological-sortings-exist-for-any-given-gr#3221401
+// Following the ideas behind this site:
+// https://math.stackexchange.com/questions/3220701/is-it-possible-to-calculate-how-many-topological-sortings-exist-for-any-given-gr#3221401
 
 #include <bits/stdc++.h>
 using namespace std;
 
 struct Vertex {
-    Vertex(int idx) : idx{idx}, in_deg{0}, is_active{true} {}
+    Vertex(int idx) : idx{idx}, in_deg{0}, is_active{true} {
+    }
 
-    bool operator<(const Vertex &u) const {return idx < u.idx;}
+    bool operator<(const Vertex &u) const {
+        return idx < u.idx;
+    }
 
     int idx;
     int in_deg;
@@ -14,11 +18,14 @@ struct Vertex {
 };
 
 struct Graph {
-    Graph() {}
+    Graph() {
+    }
 
     void add_edge(Vertex *u, Vertex *v) {
-        if (!V.count(u)) V.insert(u);
-        if (!V.count(v)) V.insert(v);
+        if (!V.count(u))
+            V.insert(u);
+        if (!V.count(v))
+            V.insert(v);
 
         if (!adj[u].count(v)) {
             adj[u].emplace(v);
@@ -36,62 +43,124 @@ struct Graph {
         u->is_active = false;
     }
 
-    int count(set<Vertex*> sources) {
-        if (V.size() <= 1) return 1;
-        if (sources.empty()) return dp[sources] = 0;
+    int count(set<Vertex *> sources) {
+        // If G has 0 vertices, it has exactly 1 topological sorting.
+        if (V.size() <= 1)
+            return 1;
 
-        if (dp.count(sources)) return dp[sources];
+        // Otherwise...  Find the source vertices of G. (These are just the
+        // vertices with indegree 0.)
 
-        set<Vertex*> aux = sources;
+        // If there are none, there are no topological sortings of G.
+        if (sources.empty())
+            return dp[sources] = 0;
 
+        if (dp.count(sources))
+            return dp[sources];
+
+        // For each source vertex s of G, let ts be the number of topological
+        // sortings of the simpler graph Gs obtained by deleting s from G.
+        set<Vertex *> aux = sources;
+
+        // The  number you want is just the sum of the ts values.
         int res = 0;
-        for (Vertex* u : sources) {
-            aux.erase(u);
-            delete_vertex(u);
-            for (Vertex* v : adj[u]) {
+
+        for (Vertex *s : sources) {
+
+            // Remove s from the graph
+            aux.erase(s);
+            delete_vertex(s);
+            // Recalculate what the sources are after removing s
+            for (Vertex *v : adj[s]) {
                 if (v->is_active) {
                     --v->in_deg;
-                    if (v->in_deg == 0) aux.emplace(v);
+                    if (v->in_deg == 0)
+                        aux.emplace(v);
                 }
             }
 
+            // Recursively calculate topo sort of smaller graph
             res += count(aux);
-            
-            add_vertex(u);
-            for (Vertex* v : adj[u]) {
+
+            // Restore s in the graph
+            add_vertex(s);
+            // Restore the in_deg of vertices and remove sources pointed by s
+            for (Vertex *v : adj[s]) {
                 if (v->is_active) {
-                    if (v->in_deg == 0) aux.erase(v);
+                    if (v->in_deg == 0)
+                        aux.erase(v);
                     ++v->in_deg;
                 }
             }
-            aux.emplace(u);
+            aux.emplace(s);
         }
 
         return dp[sources] = res;
     }
 
-    set<Vertex*> get_sources() {
-        set<Vertex*> sources;
+    set<Vertex *> get_sources() {
+        set<Vertex *> sources;
         for (Vertex *v : V) {
-            if (v->is_active && v->in_deg == 0) sources.emplace(v);
+            if (v->is_active && v->in_deg == 0)
+                sources.emplace(v);
         }
         return sources;
     }
 
-    set<Vertex*> V;
-    map<Vertex*, set<Vertex*>> adj;
-    map<set<Vertex*>, int> dp;
+    set<Vertex *> V;
+    map<Vertex *, set<Vertex *>> adj;
+    map<set<Vertex *>, int> dp;
 };
 
+/*
+Inspired by:
+
+#map = affine_map<(d0, d1, d2, d3) -> (d0, d1, d2, d3)>
+module {
+  func.func @tf2onnx(%1: tensor<?x32x32x3xf32>) -> tensor<?x10xf32> {
+    %0 = arith.constant 0 : index
+    %5 = arith.constant 0.000000e+00 : f32
+    %2 = tensor.dim %1, %0 : tensor<?x32x32x3xf32>
+    %3 = tensor.empty(%2) : tensor<?x3x32x32xf32>
+    %4 = tensor.empty(%2) : tensor<?x10xf32>
+    %6 = linalg.fill ins(%5 : f32) outs(%4 : tensor<?x10xf32>) ->
+        tensor<?x10xf32>
+    return %6 : tensor<?x10xf32>
+  }
+}
+*/
 int main() {
-    Vertex v1(0), v2(1), v3(2), v4(3), v5(4);
+    // Vertex v0(0), v1(1), v2(2), v3(3), v4(4), v5(5), v6(6);
+    // Graph G;
+    // G.add_edge(&v0, &v2);
+    // G.add_edge(&v1, &v2);
+    // G.add_edge(&v2, &v3);
+    // G.add_edge(&v2, &v4);
+    // G.add_edge(&v4, &v6);
+    // G.add_edge(&v5, &v6);
+
+    Vertex v0(0), v1(1), v2(2), v3(3);
     Graph G;
+    G.add_edge(&v0, &v2);
     G.add_edge(&v1, &v2);
-    // G.add_edge(&v1, &v4);
-    G.add_edge(&v3, &v2);
-    G.add_edge(&v3, &v4);
-    G.add_edge(&v2, &v5);
-    // G.add_edge(&v4, &v5);
+    G.add_edge(&v3, &v0);
+
+    auto srcs = G.get_sources();
+    cout << "[ ";
+    for (auto src : srcs) {
+        cout << src->idx << " ";
+    }
+    cout << "]" << endl;
 
     cout << G.count(G.get_sources()) << endl;
+
+    cout << "And what is in dp:" << endl;
+
+    for (auto &[srcs, count] : G.dp) {
+
+        cout << "topos(srcs={ ";
+        for (auto src : srcs)
+            cout << src->idx << " ";
+        cout << "}) = " << count << endl;
+    }
 }
