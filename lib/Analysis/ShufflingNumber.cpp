b7587f3581e32bbe6c219e13b8993d6d1aaa576d
bernborgess <bernborgess@outlook.com>
2026-04-07 17:20:23 -0300

    [sn] Counting operations, better formatting

--- lib/Analysis/ShufflingNumber.cpp
+++ lib/Analysis/ShufflingNumber.cpp
@@ -1,7 +1,7 @@
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/Pass/Pass.h"
-#define debug(x) llvm::outs() << #x << " = " << x << "\n"
+#define debug(x) llvm::outs() << #x << " = " << x << " "
 
 using namespace mlir;
 using namespace func;
@@ -15,29 +15,35 @@ struct ShufflingNumberPass
 
     void runOnOperation() override {
         FuncOp fn = getOperation();
+        auto fnName = fn.getName();
 
         // TODO: Will work on any function, after this base case tf2onnx works
-        if (fn.getName() != "tf2onnx") {
+        if (fnName != "tf2onnx") {
             return;
         }
 
-        llvm::outs() << "Hello World!" << "\n";
+        llvm::outs() << "### Shuffling Number of " << fnName << "\n";
+
+        int op_count = 0;
 
         fn.walk([&](Operation *op) -> WalkResult {
+            op_count++;
             std::string opName = op->getName().getStringRef().str();
-            debug(opName);
             auto operands = op->getOperands();
-            llvm::outs() << "There are " << operands.size() << " operands\n";
+            llvm::outs() << opName << " {" << operands.size() << "}[ ";
             for (auto operand : operands) {
                 auto uses = operand.getNumUses();
-                debug(uses);
+                llvm::outs() << uses << " ";
+
                 // TODO: Map out which VARIABLES are used, WHEN they are
                 // defined.
                 // https://mlir.llvm.org/docs/Tutorials/UnderstandingTheIRStructure/#traversing-the-def-use-chains
             }
-            llvm::outs() << "\n";
+            llvm::outs() << "]\n";
             return WalkResult::advance();
         });
+
+        llvm::outs() << "There were " << op_count << " operands\n";
     }
 };
 
