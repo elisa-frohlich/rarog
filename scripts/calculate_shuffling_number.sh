7e811fd8428b50c77ade1d8837ab3aa648576d13
bernborgess <bernborgess@outlook.com>
2026-04-22 09:40:03 -0300

    Verbosity and help flags for `calculate_shuffling_number.sh`

--- scripts/calculate_shuffling_number.sh
+++ scripts/calculate_shuffling_number.sh
@@ -1,28 +1,51 @@
 #!/bin/bash
 
+usage() {
+    echo "Usage: $0 [-v] [-m model_index] [-f] [-h]"
+    echo ""
+    echo "Options:"
+    echo "  -v              Enable verbose output"
+    echo "  -m model_index  Select model from 'tmp/model_<MODEL_IDX>_linalg.mlir'"
+    echo "  -f              Force recompile of rarog-opt"
+    echo "  -h              Show this help message"
+    exit 1
+}
+
 RAROG_ROOT="$(cd "$(dirname ${BASH_SOURCE[0]})/.." && pwd)"
 RAROG_OPT_PATH="${RAROG_ROOT}/build/bin/rarog-opt"
- 
-print_blue() {
-    echo -e "\e[0;34m * ${1}\e[0m"
-}
-print_red() {
-    echo -e "\e[0;31m ! ${1}\e[0m"
-}
+VERBOSE=false
+FRESH=false
+MODEL_IDX=1
 
-if [ -z $MODEL_IDX ]
-then
-    MODEL_IDX=1
+while getopts "vm:fh" OPTION; do
+  case $OPTION in
+    v)  VERBOSE=true ;;
+    m)  MODEL_IDX=$OPTARG ;;
+    f)  FRESH=true ;;
+    h)  usage ;;
+ esac
+done
+
+if [ "$VERBOSE" = true ]; then
+    echo "Verbose: ON"
+    echo "Model Index: ${MODEL_IDX}"
+    echo "Force Recompile: ${FRESH}"
+    echo ""
 fi
 
-if ! [ -f $RAROG_OPT_PATH ] || [ $FRESH ]
-then
-    print_blue "rarog-opt is not compiled. Starting compilation process..."
+print_blue() {
+    $VERBOSE && echo -e "\e[0;34m * ${1}\e[0m"
+}
+print_red() {
+    $VERBOSE && echo -e "\e[0;31m ! ${1}\e[0m"
+}
+
+if ! [ -f $RAROG_OPT_PATH ] || [ $FRESH ]; then
+    print_blue "Compiling rarog-opt..."
     cd $RAROG_ROOT
     cmake -B build . --fresh
     cmake --build build
-    if [[ $? != 0 ]]
-    then
+    if [[ $? != 0 ]]; then
         print_red "Compilation failed! Terminating..."
         exit 1
     fi
